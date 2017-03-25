869105847363371de2de3edcbcdd391aa6c18235
Baylor Rae <baylor@thecodedeli.com>
2017-03-25 18:42:29 -0500

    add support for 100, 500, and 1000

--- app/services/number_to_roman_numeral.js
+++ app/services/number_to_roman_numeral.js
@@ -27,6 +27,12 @@ export default class NumberToRomanNumeral {
 }
 
 NumberToRomanNumeral.numeralMapping = {
+  1000: "M",
+  900: "CM",
+  500: "D",
+  400: "CD",
+  100: "C",
+  90: "XC",
   50: "L",
   40: "XL",
   10: "X",
--- test/services/number_to_roman_numerals.test.js
+++ test/services/number_to_roman_numerals.test.js
@@ -52,9 +52,24 @@ test("30 should be XXX", t => {
   t.is(converter.toRomanNumeral(), "XXX");
 });
 
+test("39 should be XXXIX", t => {
+  const converter = new NumberToRomanNumeral(39);
+  t.is(converter.toRomanNumeral(), "XXXIX");
+});
+
 test("40 should be XL", t => {
   const converter = new NumberToRomanNumeral(40);
   t.is(converter.toRomanNumeral(), "XL");
+});
+
+test("43 should be XLIII", t => {
+  const converter = new NumberToRomanNumeral(43);
+  t.is(converter.toRomanNumeral(), "XLIII");
+});
+
+test("47 should be XLVII", t => {
+  const converter = new NumberToRomanNumeral(47);
+  t.is(converter.toRomanNumeral(), "XLVII");
 });
 
 test("50 should be L", t => {
@@ -65,4 +80,59 @@ test("50 should be L", t => {
 test("57 should be LVII", t => {
   const converter = new NumberToRomanNumeral(57);
   t.is(converter.toRomanNumeral(), "LVII");
+});
+
+test("90 should be XC", t => {
+  const converter = new NumberToRomanNumeral(90);
+  t.is(converter.toRomanNumeral(), "XC");
+});
+
+test("99 should be XCIX", t => {
+  const converter = new NumberToRomanNumeral(99);
+  t.is(converter.toRomanNumeral(), "XCIX");
+});
+
+test("100 should be C", t => {
+  const converter = new NumberToRomanNumeral(100);
+  t.is(converter.toRomanNumeral(), "C");
+});
+
+test("400 should be CD", t => {
+  const converter = new NumberToRomanNumeral(400);
+  t.is(converter.toRomanNumeral(), "CD");
+});
+
+test("473 should be CDLXXIII", t => {
+  const converter = new NumberToRomanNumeral(473);
+  t.is(converter.toRomanNumeral(), "CDLXXIII");
+});
+
+test("499 should be CDXCIX", t => {
+  const converter = new NumberToRomanNumeral(499);
+  t.is(converter.toRomanNumeral(), "CDXCIX");
+});
+
+test("500 should be D", t => {
+  const converter = new NumberToRomanNumeral(500);
+  t.is(converter.toRomanNumeral(), "D");
+});
+
+test("900 should be CM", t => {
+  const converter = new NumberToRomanNumeral(900);
+  t.is(converter.toRomanNumeral(), "CM");
+});
+
+test("1000 should be M", t => {
+  const converter = new NumberToRomanNumeral(1000);
+  t.is(converter.toRomanNumeral(), "M");
+});
+
+test("3832 should be MMMDCCCXXXII", t => {
+  const converter = new NumberToRomanNumeral(3832);
+  t.is(converter.toRomanNumeral(), "MMMDCCCXXXII");
+});
+
+test("3999 should be MMMCMXCIX", t => {
+  const converter = new NumberToRomanNumeral(3999);
+  t.is(converter.toRomanNumeral(), "MMMCMXCIX");
 });
